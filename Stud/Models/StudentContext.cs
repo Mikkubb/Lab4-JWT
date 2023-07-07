39559065b6388486c8e816ec4f56d641b5027989
Jakub Michalski <Jakub.Michalski@microsoft.wsei.edu.pl>
2023-07-07 16:00:13 +0200

    Dodaj pliki projektów.

---
+++ Stud/Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Stud.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; } = null!;
+    }
+
+}
